005b71c4d5bcd6f560669456e3b2b3ef3999a0f8
Gjorge Gogov <gjorge.gogov@hotmail.com>
2024-08-02 19:02:43 +0200

    too much responsibility example

--- 1_SOLID/1_SingleResponsibility/SingleResponsibility/SingleResponsibility/Program.cs
+++ 1_SOLID/1_SingleResponsibility/SingleResponsibility/SingleResponsibility/Program.cs
@@ -1,10 +1,54 @@
-﻿namespace SingleResponsibility
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace SingleResponsibility
 {
-	internal class Program
+	public class Journal
+	{
+		private readonly List<string> entries = new List<string>();
+		private static int count = 0;
+		public int AddEntry(string text)
+		{
+			entries.Add($"{++count}: {text}");
+			return count; // memento pattern
+		}
+		public void RemoveEntry(int index)
+		{
+			entries.RemoveAt(index); //unstable
+		}
+		public override string ToString()
+		{
+			return string.Join(Environment.NewLine, entries);
+		}
+
+		//Too much
+
+		public void Save(string filename)
+		{
+			File.WriteAllText(filename,ToString());
+		}
+		public static Journal Load(string filename)
+		{
+
+		}
+		public void Load(Uri uri)
+		{
+
+		}
+	}
+
+	public class Demo
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			var j = new Journal();
+			j.AddEntry("I cried today");
+			j.AddEntry("I solved a bug");
+			WriteLine(j); //calls ToString
 		}
 	}
 }
