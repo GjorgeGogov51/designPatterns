a45167c87392e653acec351cce052e6376f987b7
Gjorge Gogov <gjorge.gogov@hotmail.com>
2024-08-03 19:23:36 +0200

    liskov substitution setup

---
+++ 1_SOLID/3_LiskovSubstitution/LiskovSubstitution/LiskovSubstitution/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace LiskovSubstitution
+{
+	public class Demo
+	{
+		public class Rectangle
+		{
+			public int Width { get; set; }
+			public int Height { get; set; }
+            public Rectangle()
+            {
+                
+            }
+			public Rectangle(int width, int height)
+			{
+				Width = width;
+				Height = height;
+			}
+			//Formatting member
+			public override string ToString()
+			{
+				return $"{nameof(Width)}: {Width}, {nameof(Height)}: {Height}";
+			}
+
+		}
+		static void Main(string[] args)
+		{
+			Rectangle rc = new Rectangle();
+		}
+	}
+}
