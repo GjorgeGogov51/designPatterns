cebb1f60a0da54a2ec5c8a303b63a0b695b87598
Gjorge Gogov <gjorge.gogov@hotmail.com>
2024-08-04 18:38:13 +0200

    dependency invversion setup

---
+++ 1_SOLID/5_DependencyInversion/DependencyInversion/DependencyInversion/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace DependencyInversion
+{
+	public enum Relationship
+	{
+		Parent, Child, Sibling
+	}
+	public class Person
+	{
+		public string Name;
+	}
+
+	//low-level
+	public class Relationships
+	{
+		private List<(Person, Relationship, Person)> Relations
+			= new List<(Person, Relationship, Person)> ();
+
+		public void AddParentAndChild(Person parent, Person child)
+		{
+			Relations.Add((parent, Relationship.Parent, child));
+			Relations.Add((child, Relationship.Child, parent));
+		}
+	}
+	//high-level
+	public class Research
+	{
+		static void Main(string[] args)
+		{
+			var parent = new Person() { Name = "John" };
+			var child1 = new Person() { Name = "Chris" };
+			var child2 = new Person() { Name = "Mary" };
+
+			var relationships = new Relationships();
+			relationships.AddParentAndChild(parent, child1);
+			relationships.AddParentAndChild (parent, child2);
+		}
+	}
+}
