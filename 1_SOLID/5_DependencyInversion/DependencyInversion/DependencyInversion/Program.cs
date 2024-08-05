e321b1e9ba6b174d4e67302780d8fccb891c530c
Gjorge Gogov <gjorge.gogov@hotmail.com>
2024-08-05 20:01:11 +0200

    bad dependency inversion example

--- 1_SOLID/5_DependencyInversion/DependencyInversion/DependencyInversion/Program.cs
+++ 1_SOLID/5_DependencyInversion/DependencyInversion/DependencyInversion/Program.cs
@@ -29,12 +29,25 @@ namespace DependencyInversion
 			Relations.Add((parent, Relationship.Parent, child));
 			Relations.Add((child, Relationship.Child, parent));
 		}
+		public List<(Person, Relationship, Person)> relations => Relations;
 	}
 	//high-level
 	public class Research
 	{
+		public Research(Relationships relationships)
+		{
+			var relations = relationships.relations;
+			foreach (var r in relations.Where(
+				x => x.Item1.Name == "John" &&
+				x.Item2 == Relationship.Parent
+				))
+			{
+				WriteLine($"John has a child called {r.Item3.Name}");
+			}
+		}
 		static void Main(string[] args)
 		{
+			
 			var parent = new Person() { Name = "John" };
 			var child1 = new Person() { Name = "Chris" };
 			var child2 = new Person() { Name = "Mary" };
@@ -42,6 +55,8 @@ namespace DependencyInversion
 			var relationships = new Relationships();
 			relationships.AddParentAndChild(parent, child1);
 			relationships.AddParentAndChild (parent, child2);
+
+			new Research(relationships);
 		}
 	}
 }
