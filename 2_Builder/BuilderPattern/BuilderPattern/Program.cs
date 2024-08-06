f6260ab3281deb473697d56114ae00c6a50c50fc
Gjorge Gogov <gjorge.gogov@hotmail.com>
2024-08-06 13:55:15 +0200

    html element

--- 2_Builder/BuilderPattern/BuilderPattern/Program.cs
+++ 2_Builder/BuilderPattern/BuilderPattern/Program.cs
@@ -9,6 +9,42 @@ using static System.Console;
 
 namespace BuilderPattern
 {
+	public class HtmlElement
+	{
+		public string Name, Text;
+		public List<HtmlElement> Elements = new List<HtmlElement>();
+		private const int indentSize = 2;
+
+		public HtmlElement(string name, string text)
+		{
+			Name = name ?? throw new ArgumentNullException(paramName: nameof(name));
+			Text = text ?? throw new ArgumentNullException(paramName: nameof(text));
+		}
+
+		private string ToStringImpl(int indent)
+		{
+			var sb = new StringBuilder();
+			var i = new string(' ', indentSize * indent);
+			sb.AppendLine($"{i}<{Name}>");
+
+			if(!string.IsNullOrWhiteSpace(Text))
+			{
+				sb.Append(new string(' ', indentSize * (indent+1)));
+				sb.AppendLine(Text);
+			}
+			foreach(var e in Elements)
+			{
+				sb.Append(e.ToStringImpl(indent+1));
+			}
+			sb.AppendLine($"{i}</{Name}>");
+			return sb.ToString();
+		}
+
+		public override string ToString()
+		{
+			return ToStringImpl(0);
+		}
+	}
 	public class Demo
 	{
 		static void Main(string[] args)
@@ -30,6 +66,7 @@ namespace BuilderPattern
 			sb.Append("</ul>");
 			WriteLine(sb);
 
+			//WriteLine(new HtmlElement("ime", "tekst"));
 		}
 	}
 }
