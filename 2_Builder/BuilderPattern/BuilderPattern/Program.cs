02a922bdaf80dd55560621f98c4bee4388b5b4a3
Gjorge Gogov <gjorge.gogov@hotmail.com>
2024-08-06 11:56:23 +0200

    build setup

---
+++ 2_Builder/BuilderPattern/BuilderPattern/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace BuilderPattern
+{
+	public class Demo
+	{
+		static void Main(string[] args)
+		{
+			var hello = "hello";
+			var sb = new StringBuilder();
+			sb.Append("<p>");
+			sb.Append(hello);
+			sb.Append("</p>");
+			WriteLine(sb);
+
+			var words = new[] { "hello", "world" };
+			sb.Clear();
+			sb.Append("<ul>");
+			foreach(var word in words) 
+			{
+				sb.AppendFormat($"<li>{word}</li>");
+			}
+			sb.Append("</ul>");
+			WriteLine(sb);
+
+		}
+	}
+}
