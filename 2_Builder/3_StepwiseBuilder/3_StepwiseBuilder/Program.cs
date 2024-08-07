3e6fc472970e4f53bafb92dc225a117d93e35911
Gjorge Gogov <gjorge.gogov@hotmail.com>
2024-08-07 19:47:55 +0200

    stepwise builder setup

---
+++ 2_Builder/3_StepwiseBuilder/3_StepwiseBuilder/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace _3_StepwiseBuilder
+{
+	public enum CarType
+	{
+		Sedan, Crossover
+	}
+	public class Car
+	{
+		//Car type determines Wheel Size, so we need a separate interface for each
+		public CarType type;
+		public int WheelSize;
+	}
+	public class Demo
+	{
+		static void Main(string[] args)
+		{
+
+		}
+	}
+}
