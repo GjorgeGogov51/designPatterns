998d5d45d6dbb80fd93f40299ec2a7f123d96843
Gjorge Gogov <gjorge.gogov@hotmail.com>
2024-08-07 12:34:36 +0200

    fluent builder setup

---
+++ 2_Builder/2_FluentBuilderInheritanceWithRecursiveGenerics/2_FluentBuilderInheritanceWithRecursiveGenerics/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace _2_FluentBuilderInheritanceWithRecursiveGenerics
+{
+	public class Person
+	{
+		public string Name;
+		public string Position;
+
+		public override string? ToString()
+		{
+			return $"{nameof(Name)}: {Name}, {nameof(Position)}: {Position}";
+		}
+	}
+
+	public class PersonInfoBuilder
+	{
+		protected Person person = new Person();
+
+		public PersonInfoBuilder Called(string name)
+		{
+			person.Name = name;
+			return this;
+		}
+	}
+	internal class Program
+	{
+		public static void Main(string[] args)
+		{
+			
+		}
+	}
+}
