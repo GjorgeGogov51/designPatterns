948334675a303fb05b50bde7ba0bc2b747172f4f
Gjorge Gogov <gjorge.gogov@hotmail.com>
2024-08-17 10:59:24 +0200

    ut setup

---
+++ x_UT/1_UnitTests/UnitTests/Program.cs
@@ -0,0 +1,12 @@
+﻿using UnitTests.Tests;
+
+namespace UnitTests
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			WorldsDumbestFunctionTests.WorldsDumbestFunction_ReturnsPikachuIfZero_ReturnsString();
+		}
+	}
+}
---
+++ x_UT/1_UnitTests/UnitTests/Tests/WorldsDumbestFunctionTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.Tests
+{
+	public static class WorldsDumbestFunctionTests
+	{
+		//Naming Convention - ClassName_MethodName_ExpectedResults
+		public static void WorldsDumbestFunction_ReturnsPikachuIfZero_ReturnsString()
+		{
+			try
+			{
+				//Arrange - Go get your variables, classes, functions
+				int num = 0;
+				WorldsDumbestFunction worldsDumbest = new WorldsDumbestFunction();
+
+				//Act - Execute this function
+				string result = worldsDumbest.ReturnsPikachuIfZero(num);
+
+				//Assert - Whatever is returned, is what you want
+				if (result.Equals("PIKACHU!"))
+				{
+					Console.WriteLine("PASSED: WorldsDumbestFunction_ReturnsPikachuIfZero_ReturnsString");
+				}
+				else
+				{
+					Console.WriteLine("FAILED: WorldsDumbestFunction_ReturnsPikachuIfZero_ReturnsString");
+				}
+
+			}
+			catch (Exception ex) 
+			{
+				Console.WriteLine(ex);
+			}
+		}
+
+	}
+}
