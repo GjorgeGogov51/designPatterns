30ab2cc3104eaf959a71fb69ccd83a90522188e2
Gjorge Gogov <gjorge.gogov@hotmail.com>
2024-08-21 11:10:40 +0200

    a bit cleaner

---
+++ 3_Factory/FactoryPattern/2_FactoryMethod/Demo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_FactoryMethod
+{
+	public class Point
+	{
+		private double x, y;
+		public Point(double x, double y)
+		{
+			this.x = x;
+			this.y = y;
+		}
+	public class Demo
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("hemlo");
+		}
+	}
+}
