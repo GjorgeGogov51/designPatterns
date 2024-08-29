fad2c1c64a5d3d031b41237675816eaf7e32a340
Gjorge Gogov <gjorge.gogov@hotmail.com>
2024-08-29 17:25:46 +0200

    remind

---
+++ x_AS/ConsoleApp1/ConsoleApp1/asTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	internal class asTest
+	{
+		
+		public async Task izvrsi()
+		{
+			await Task.WhenAll(hullo(), ello());
+		}
+		public async Task hullo()
+		{
+			int i = 0;
+			while (i < 100)
+			{
+				await Task.Delay(1000);
+				Console.WriteLine($"hullo {i}");
+				i++;
+			}
+			return;
+		}
+		public async static Task ello()
+		{
+			int i = 0;
+			while (i < 100)
+			{
+				await Task.Delay(1000);
+				Console.WriteLine($"ello {i}");
+				i++;
+			}
+			return;
+		}
+	}
+}
