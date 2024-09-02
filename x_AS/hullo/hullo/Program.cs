45fcee8cf3eaaa5250e3e65eff0798ebe4426161
Gjorge Gogov <gjorge.gogov@hotmail.com>
2024-09-02 10:52:23 +0200

    alt multi

---
+++ x_AS/hullo/hullo/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace hullo
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Thread thread = new Thread(CountNumbers);
+
+			thread.Start();
+
+			for (int i = 0; i < 5; i++)
+			{
+				Console.WriteLine("Main thread: " + i);
+				Thread.Sleep(500);
+			}
+			thread.Join();
+
+			Console.WriteLine("Thread execution completed.");
+		}
+
+		public static void CountNumbers()
+		{
+			for (int i = 0; i < 5; i++)
+			{
+				Console.WriteLine("Second thread: " + i);
+				Thread.Sleep(1000);
+			}
+		}
+	}
+
+	
+}
