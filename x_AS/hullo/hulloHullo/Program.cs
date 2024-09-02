3ffc0f39900de5c4486a49b9e57ccd49e1727d5e
Gjorge Gogov <gjorge.gogov@hotmail.com>
2024-09-02 11:08:19 +0200

    comparison

---
+++ x_AS/hullo/hulloHullo/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace hulloHullo
+{
+	internal class Program
+	{
+		static async void Main(string[] args)
+		{
+			Task task = CountNumbersAsync();
+
+			for (int i = 0; i < 5; i++)
+			{
+				Console.WriteLine("Main thread: " + i);
+				await Task.Delay(500);
+			}
+			await task;
+
+			Console.WriteLine("Task execution completed.");
+		}
+
+		static async Task CountNumbersAsync()
+		{
+			for (int i = 0; i < 5; i++)
+			{
+				Console.WriteLine("Task: " + i);
+				await Task.Delay(1000);
+			}
+		}
+	}
+}
