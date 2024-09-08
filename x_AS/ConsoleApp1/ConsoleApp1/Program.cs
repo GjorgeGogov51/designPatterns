3b21572bd2cc30741d521c6c69bf9beb3d8ccb96
Gjorge Gogov <gjorge.gogov@hotmail.com>
2024-09-08 12:11:08 +0200

    notes async-threads

--- x_AS/ConsoleApp1/ConsoleApp1/Program.cs
+++ x_AS/ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,7 +4,13 @@
 	{
 		public static void Main(string[] args)
 		{
-			new asTest().izvrsi().Wait();
+			//Concurrency - single core, time sharing
+			//Parallelism - multiple cores, multiple threads
+
+			//Async doesn't create new threads (with synchronization context)
+			//State machine - separated code segment execution
+
+			new asTest().izvrsi().Wait(); // or don't
 		}
 	}
 }
